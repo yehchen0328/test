8e29f92e40449b424534b31c38f973324dc35f1a
yehchen0328 <99935197+yehchen0328@users.noreply.github.com>
2023-09-20 16:34:59 +0800

    Add files via upload

--- Topic/app.py
+++ Topic/app.py
@@ -1,15 +1,37 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from flask_mail import Mail, Message
+import random
+import string
 import re
+import time
+
+
+
 
 
 app = Flask(__name__, template_folder='C:\\Users\\a0952\\Topic\\templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:user1234@localhost/name'
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-db = SQLAlchemy(app)
+'''
+#hotmail 設置
+app.config.update(
 
+    MAIL_SERVER='smtp.live.com',
+    MAIL_PROT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME='Your Hotmail Count',
+    MAIL_PASSWORD='Your Hotmail Password'
+
+)'''
+
+db = SQLAlchemy(app)
+'''
+mail = Mail(app)
+mail.int_app(app)
+'''
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Email = db.Column(db.String(200), unique=True, nullable=False)
@@ -70,12 +92,52 @@ def register():
             return render_template('login.html')
 
     return render_template('login.html')
+'''
+#hotmail
+@app.route("/message")
+def hotmail():
+    msg_title = 'Hello '
+    '''
+
+#忘記密碼
+@app.route('/reset', methods=['GET','POST'])
+def reset():
+    Email = request.form.get('Email')
+    token = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+    msg = Message('Reset Your Password', sender=request.form.get('sender_email', 'default@example.com'), recipients=[Email])
+    msg.body = f"Here's your password reset token: {token}"
+    Mail.send(msg)
+    
+    flash('Password reset token has been sent to your email,','success')
+    return redirect(url_for('forgot_password'))
 
 
 #功能主頁面
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+
+#倒數計時器
+def countdown(num_of_secs):
+    while num_of_secs:
+        m, s = divmod(num_of_secs, 60)
+        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+        print(min_sec_format, end='\r')  # \r更新倒數計時
+        time.sleep(1)
+        num_of_secs -= 1
+
+    print('Countdown finished.    ')  # 額外的空格是為了覆蓋之前的倒計時，以確保它顯示正確
+
+
+time_input = input('Input time in MM:SS format: ')
+
+minutes, seconds = map(int, time_input.split(':'))# 使用 split() 方法根據 ":" 分割輸入的字串，得到分鐘和秒數
+
+total_seconds = minutes*60+seconds  # 計算總秒數
+
+countdown(total_seconds)
 
 if __name__=="__main__":
     app.run(debug=True)
